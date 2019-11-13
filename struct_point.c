b05e96c1c57bea3fc343ccbc66841a3c61e53017
jzzh <jzzh@szbaijie.cn>
2019-11-13 11:31:53 +0800

    change struct_point.c use * value

--- struct_point.c
+++ struct_point.c
@@ -4,19 +4,25 @@
 
 struct study_s {
 	int num;
+	int *num_2;
 	char name[10];
 };
 
 int main ()
 {
 	struct study_s *ip=NULL;        /* 指针变量的声明 */
+	int var = 100;
 
 	ip = (struct study_s *)malloc(sizeof(struct study_s));	
 	ip->num = 2;
+	ip->num_2 = &var;
 	printf("Address of var variable: %d\n", ip->num);
+	printf("Address of var variable: %p\n", ip->num_2);
 
 	/* 在指针变量中存储的地址 */
 	printf("Address stored in ip variable: %p\n", &ip->num );
+	printf("Address stored in ip variable: %p\n", &ip->num_2 );
+	printf("Address stored in ip variable: %d\n", *ip->num_2 );
 	printf("Address stored in ip variable: %p\n", &ip);
 	printf("Address stored in ip variable: %p\n", ip);
 
