092613e166d8b25bc44d9c83e69df6db40d5da62
jizizihe <jizizihe@163.com>
2020-06-13 10:02:24 +0800

    add array and point test

--- point/build.sh
+++ point/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-gcc point_and_address_1.c -o point_and_address_1.o
-gcc point_arguments_2.c -o point_arguments_2.o
-gcc second_pointer_3.c -o second_pointer_3.o
+#gcc point_and_address_1.c -o point_and_address_1.o
+#gcc point_arguments_2.c -o point_arguments_2.o
+#gcc second_pointer_3.c -o second_pointer_3.o
+gcc array_and_point_4.c -o array_and_point_4.o
